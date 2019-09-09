ede678927918f4ed2dbf2499f1d19ac7c0f218b2
phungdaihiep <thanghiep007@gmail.com>
2019-09-09 12:08:40 +0700

    load dc googleSignin

---
+++ frontend/src/androidClientid.js
@@ -0,0 +1,1 @@
+export const OauthKey = "226674535243-rtb09m4gllm6htasbhuq34h11udvv6ki.apps.googleusercontent.com";
--- frontend/src/screens/DangNhap.js
+++ frontend/src/screens/DangNhap.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { SocialIcon } from 'react-native-elements';
+import {OauthKey} from '../androidClientid';
+import * as Expo from 'expo';
+import * as Google from 'expo-google-app-auth';
+
 
 
 export default class DangNhap extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-  
-  render() {
-    return (
-      <View>
-        <Text> DangNhap </Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    signInWithGoogleAsync = async() => {
+        try {
+          const result = await Google.logInAsync({
+            androidClientId: OauthKey,
+            // iosClientId: YOUR_CLIENT_ID_HERE,
+            scopes: ['profile', 'email'],
+          });
+          console.log(result)
+      
+          if (result.type === 'success') {
+            // return result.accessToken;
+            console.log(result.accessToken)
+          } else {
+            // return { cancelled: true };
+            console.log("cancel")
+          }
+        } catch (e) {
+        //   return { error: true };
+        console.log(e)
+        }
+      }
+
+    render() {
+        return (
+            <View style={styles.container} >
+                <SocialIcon
+                    title='Sign In With Google'
+                    button
+                    type='google'
+                    onPress = {this.signInWithGoogleAsync}
+                />
+            </View>
+
+
+        );
+    }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center"
+    }
+});
